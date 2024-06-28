9bab848b4c7acb727cf226bfc5300cf4a9e3e479
kevindermot99 <mbonimpayekevin@gmail.com>
2024-06-28 11:14:48 +0200

    animated welcome screens

--- src/Pages/Welcome1.jsx
+++ src/Pages/Welcome1.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import Logo from "../assets/logo.png";
 import BusStop from "../assets/bus-Stop.png";
 import { GrFormNext } from "react-icons/gr";
 import { Link } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 
 function Welcome1() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // Perform any actions before navigating, if needed
+
+    // Redirect to '/feature2'
+    navigate("/feature2");
+  };
   return (
     <div className="h-[100vh] w-full overflow-x-clip overflow-y-auto flex flex-col items-center justify-center max-sm:justify-end p-8 max-sm:p-4 bg-white dark:bg-white">
       <div className="fixed top-0 left-0 p-8 max-[250px]:bg-white dark:bg-white w-full h-fit flex justify-between items-center select-none ">
@@ -33,32 +43,39 @@ function Welcome1() {
         </div>
       </div>
 
-      <div className="max-w-[1100px] h-fit max-sm:h-full max-sm:pt-[300px] flex flex-col items-center justify-end max-sm:gap-10 py-10 max-sm:py-3 mx-auto">
-        <div className="flex items-center justify-center">
-          <img src={Logo} className="h-6 pt-0" />
-          <h1 className=" font-bold text-[22px] tracking-tighter text-main-color pr-3  ">
-            gerayo.
-          </h1>
-        </div>
-        <div className="flex items-center justify-center flex-col select-none pointer-events-none">
-          <img src={BusStop} className="h-auto w-full max-w-[500px]" />
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <h1 className="font-bold tracking-tighter text-2xl text-center text-dark-text/90 line-clamp-2 max-w-[350px]">
-            Check the next bus arrival time
-          </h1>
-          <p className="tracking-tight text-sm text-center text-dark-text/80  max-w-[400px] font-semibold">
-            Enter your location and route for real-time bus updates and never
-            miss your ride.
-          </p>
-
-          <Link
-            to={`/feature2`}
-            className="bg-main-color my-5 text-white font-bold text-base transition active:scale-90 h-[50px] w-full max-w-[250px] rounded-[20px] flex items-center justify-center"
-          >
-            Continue
-          </Link>
-        </div>
+      <div className="max-w-[1100px] w-full h-fit max-sm:h-full max-sm:pt-[300px] flex flex-col items-center justify-end max-sm:gap-10 py-10 max-sm:py-3 mx-auto">
+        <Fade
+        duration={500}
+        direction="up"
+        cascade
+        damping={0.3}
+        className="w-full flex flex-col items-center justify-center"
+        >
+          <div className="flex items-center justify-center ">
+            <img src={Logo} className="h-6" />
+            <h1 className=" font-bold text-[22px] tracking-tighter text-main-color ">
+              gerayo.
+            </h1>
+          </div>
+          <div className="flex items-center justify-center flex-col select-none pointer-events-none">
+            <img src={BusStop} className="h-auto w-full max-w-[500px]" />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="font-bold tracking-tighter text-2xl text-center text-dark-text/90 line-clamp-2 max-w-[350px]">
+              Check the next bus arrival time
+            </h1>
+            <p className="tracking-tight text-sm text-center text-dark-text/80  max-w-[400px] font-semibold">
+              Enter your location and route for real-time bus updates and never
+              miss your ride.
+            </p>
+          </div>
+          <button
+              onClick={handleClick}
+              className="bg-main-color my-5 outline-none text-white font-bold text-base transition active:scale-90 h-[50px] w-full max-w-[250px] rounded-[20px] flex items-center justify-center"
+            >
+              Continue
+            </button>
+        </Fade>
       </div>
     </div>
   );
--- src/Pages/Welcome2.jsx
+++ src/Pages/Welcome2.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import Logo from "../assets/logo.png";
 import Travelers from "../assets/travelers.png";
 import { GrFormNext } from "react-icons/gr";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Fade } from "react-awesome-reveal";
 
 function Welcome2() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    // Perform any actions before navigating, if needed
+
+    // Redirect to '/feature2'
+    navigate("/feature2");
+  };
   return (
     <div className="h-[100vh] w-full overflow-x-clip overflow-y-auto flex flex-col items-center justify-center max-sm:justify-end p-8 max-sm:p-4 bg-white dark:bg-white">
       <div className="fixed top-0 left-0 p-8 max-[250px]:bg-white dark:bg-white w-full h-fit flex justify-between items-center select-none ">
@@ -34,28 +43,39 @@ function Welcome2() {
       </div>
 
       <div className="max-w-[1100px] h-fit max-sm:h-full flex flex-col items-center justify-end max-sm:gap-10 py-10 max-sm:py-3 mx-auto">
-        <div className="flex items-center justify-center">
-          <img src={Logo} className="h-6 pt-0" />
-          <h1 className=" font-bold text-[22px] tracking-tighter text-main-color pr-3  ">
-            gerayo.
-          </h1>
-        </div>
-        <div className="flex items-center justify-center flex-col select-none pointer-events-none">
-          <img src={Travelers} className="h-auto w-full max-w-[355px]" />
-        </div>
-        <div className="flex flex-col items-center justify-center gap-2">
-          <h1 className="font-bold tracking-tighter text-2xl text-center text-dark-text/90 line-clamp-2 max-w-[350px]">
-            Purchase bus tickets online.
-          </h1>
-          <p className="tracking-tight text-sm text-center text-dark-text/80  max-w-[400px] font-semibold">
-            Buy your tickets while sipping coffee, chilling in your office move
-            nothing but your fingers!
-          </p>
+        <Fade
+          duration={500}
+          direction="up"
+          cascade
+          damping={0.3}
+          className="w-full flex flex-col items-center justify-center"
+        >
+          <div className="flex items-center justify-center">
+            <img src={Logo} className="h-6 pt-0" />
+            <h1 className=" font-bold text-[22px] tracking-tighter text-main-color pr-3  ">
+              gerayo.
+            </h1>
+          </div>
+          <div className="flex items-center justify-center flex-col select-none pointer-events-none">
+            <img src={Travelers} className="h-auto w-full max-w-[355px]" />
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2">
+            <h1 className="font-bold tracking-tighter text-2xl text-center text-dark-text/90 line-clamp-2 max-w-[350px]">
+              Purchase bus tickets online.
+            </h1>
+            <p className="tracking-tight text-sm text-center text-dark-text/80  max-w-[400px] font-semibold">
+              Buy your tickets while sipping coffee, chilling in your office
+              move nothing but your fingers!
+            </p>
+          </div>
 
-          <button className="bg-main-color my-5 text-white font-bold text-base transition active:scale-90 h-[50px] w-full max-w-[250px] rounded-[20px] ">
+          <button
+            onClick={handleClick}
+            className="bg-main-color my-5 text-white font-bold text-base transition active:scale-90 h-[50px] w-full max-w-[250px] rounded-[20px] "
+          >
             Continue
           </button>
-        </div>
+        </Fade>
       </div>
     </div>
   );
