e7275e10585680da153dce1bc3003c7201e78c4b
kevindermot99 <mbonimpayekevin@gmail.com>
2024-06-30 00:53:59 +0200

    login as guest complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,26 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Welcome1 from "./Pages/Welcome1";
-import Welcome2 from "./Pages/Welcome2";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
-import Center from "./Pages/Center";
+import Track from "./Pages/Track";
 
 function App() {
   const [visited, setVisited] = useState(false);
 
   useEffect(() => {
-    const visitedAs = localStorage.getItem('visitedAs')
-    if(visitedAs){
-      setVisited(true)
+    const visitedAs = localStorage.getItem("visitedAs");
+    if (visitedAs) {
+      setVisited(true);
     }
-  }, [])
+  }, []);
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {visited ? (
-            <>
-              <Route path="/" element={<Center />} />
-            </>
-          ) : (
-            <>
-              <Route path="/" element={<Welcome1 />} />
-              <Route path="/feature2" element={<Welcome2 />} />
-            </>
-          )}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Track />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/Pages/Track.jsx
+++ src/Pages/Track.jsx
@@ -11,29 +11,28 @@ import { Link, useNavigate } from "react-router-dom";
 import { SlOptions } from "react-icons/sl";
 import Ticket from "../Components/Ticket";
 
-function Center() {
-    const [visited, setVisited] = useState(null);
-    const navigate = useNavigate()
+function Track() {
+  const [visited, setVisited] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    setVisited(localStorage.getItem('visitedAs'))
-  }, [])
-
-  if(visited !== null){
-      navigate('/')
+    const visitadAs = localStorage.getItem("visitedAs")
+    if (!visitadAs) {
+      navigate("/login");
     }
+  }, []);
+
+  
   return (
     <div className="w-full h-full flex min-h-screen bg-body-color-light dark:bg-body-color-dark  overflow-y-hidden max-h-dvh ">
       <Sidebar />
       <div className=" w-full min-h-full">
         <Navbar title="Home" />
-        
-        <div className="gridRespo p-5 gap-2 overflow-y-auto max-h-[90vh] pb-20">
-          
-        </div>
+
+        <div className="gridRespo p-5 gap-2 overflow-y-auto max-h-[90vh] pb-20"></div>
       </div>
     </div>
   );
 }
 
-export default Center;
+export default Track;
