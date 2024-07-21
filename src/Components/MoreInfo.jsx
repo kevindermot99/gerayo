b66ddd2042bf1ed46f055c9bce575eb5b943909e
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-21 19:49:17 +0200

    more info & notifications dark

--- src/Components/MoreInfo.jsx
+++ src/Components/MoreInfo.jsx
@@ -14,7 +14,7 @@ function MoreInfo({ hide, id }) {
   const [load, setLoad] = useState(true);
   const [image, setImage] = useState("");
   const [showImageFull, setShowImageFull] = useState(false);
-  const pathname = useLocation()
+  const pathname = useLocation();
 
   useEffect(() => {
     setAnimate(true);
@@ -57,47 +57,47 @@ function MoreInfo({ hide, id }) {
   }, [pathname]);
 
   return (
-    <>
-    {/* full screen image */}
-    <div
-          className={`fixed top-0 left-0 w-full h-full z-50 flex flex-col items-center justify-center ${
-            showImageFull ? "visible " : "invisible "
+    <div className="fixed top-0 bottom-0 right-0 left-0 m-auto w-full h-full z-40">
+      {/* full screen image */}
+      {/* <div
+        className={`fixed top-0 left-0 w-full h-full z-50 flex flex-col items-center justify-center ${
+          showImageFull ? "visible " : "invisible "
+        }`}
+      >
+        <div
+          onClick={() => setShowImageFull(false)}
+          className={`absolute top-0 left-0 bg-black/10 w-full h-full backdrop-blur-sm transition-all ease-out ${
+            showImageFull ? "opacity-100 " : "opacity-0 "
+          } `}
+        ></div>
+        <img
+          src={image}
+          className={` max-h-[70%] h-[220px] brightness-[110%] min-w-[300px] max-w-[80%] object-cover shadow rounded-xl z-10 select-none transition-all ease-in ${
+            showImageFull ? "opacity-100 " : "opacity-0 scale-95  "
           }`}
-        >
-          <div
-            onClick={() => setShowImageFull(false)}
-            className={`absolute top-0 left-0 bg-black/10 w-full h-full backdrop-blur-sm transition-all ease-out ${
-              showImageFull ? "opacity-100 " : "opacity-0 "
-            } `}
-          ></div>
-          <img
-            src={image}
-            className={` max-h-[70%] h-[220px] brightness-[110%] min-w-[300px] max-w-[80%] object-cover shadow rounded-xl z-10 select-none transition-all ease-in ${
-              showImageFull ? "opacity-100 " : "opacity-0 scale-95  "
-            }`}
-          />
-        </div>
-      <div
+        />
+      </div> */}
+      {/* <div
         onClick={hideNotificationPopup}
         className={`w-full h-full fixed top-0 left-0 bg-stone-600/10 z-40 transition-all ease-in-out duration-300 ${
           animate ? "opacity-100" : "opacity-0"
         } `}
-      ></div>
+      ></div> */}
       <div
-        className={`fixed top-0 right-0 bg-white text-dark-text w-full max-w-[500px] h-full z-40 p-4 flex flex-col transition-all ease-in-out duration-200  ${
+        className={`absolute right-0 bg-white dark:bg-body-color-dark shadow-2xl text-dark-text dark:text-light-text w-full max-w-[500px] h-full z-40 p-4 flex flex-col transition-all ease-in-out duration-200  ${
           animate
             ? " translate-x-0 opacity-100"
             : " translate-x-full opacity-0 "
         }`}
       >
-        <div className="flex h-fit items-center justify-start gap-4">
+        <div className="flex h-fit items-center justify-start gap-4 pb-3">
           <button
             onClick={hideNotificationPopup}
             className="active:scale-75 transition flex items-center justify-center "
           >
             <IoArrowBackOutline className="text-2xl" />
           </button>
-          <h1 className="text-lg tracking-tight font-bold capitalize ">
+          <h1 className="text-lg tracking-tight font-bold capitalize  ">
             More info
           </h1>
         </div>
@@ -105,7 +105,7 @@ function MoreInfo({ hide, id }) {
         {/* no notification */}
         {load ? (
           <div className="w-full h-full flex items-center justify-center pb-0">
-          <CgSpinner className="animate-spinLoader text-2xl text-dark-text/50" />
+            <CgSpinner className="animate-spinLoader text-2xl text-dark-text/50 dark:text-light-text" />
           </div>
         ) : (
           <div className="h-full w-full max-w-[500px] flex flex-col gap-5 max-md:px-3 px-7 pt-8 pb-14 max-md:pb-32 overflow-y-auto">
@@ -115,14 +115,14 @@ function MoreInfo({ hide, id }) {
                   From: {openedJourney.from}
                 </h1>
                 <img
-                onClick={() => showImage(imageFrom.image)}
+                  onClick={() => showImage(imageFrom.image)}
                   src={imageFrom.image}
                   className="w-full h-[130px] cursor-pointer bg-stone-100 rounded-xl shadow-md object-cover object-bottom"
                 />
                 <a
                   href={`https://www.google.com/maps/place/${openedJourney.from}`}
                   target="_blank"
-                  className="group p-2 rounded-xl bg-white/90 transition active:scale-105 backdrop-blur-sm shadow-lg absolute bottom-2 right-2"
+                  className="group p-2 rounded-xl bg-white/90 dark:bg-container-dark transition active:scale-105 backdrop-blur-sm shadow-lg absolute bottom-2 right-2"
                 >
                   <FaMapLocationDot className="text-sm transition group-hover:text-main-color" />
                 </a>
@@ -139,7 +139,7 @@ function MoreInfo({ hide, id }) {
                 <a
                   href={`https://www.google.com/maps/place/${openedJourney.to}`}
                   target="_blank"
-                  className="group p-2 rounded-xl bg-white/90 transition active:scale-105 backdrop-blur-sm shadow-lg absolute bottom-2 right-2"
+                  className="group p-2 rounded-xl bg-white/90 dark:bg-container-dark transition active:scale-105 backdrop-blur-sm shadow-lg absolute bottom-2 right-2"
                 >
                   <FaMapLocationDot className="text-sm transition group-hover:text-main-color" />
                 </a>
@@ -150,17 +150,20 @@ function MoreInfo({ hide, id }) {
             <div className="flex items-start justify-start gap-2">
               <div className="h-full w-fit flex items-center justify-between flex-col gap-1">
                 <span>
-                  <PiBus className=" text-xl text-dark-text/80" />
+                  <PiBus className=" text-xl text-dark-text/80 dark:text-light-text/80" />
                 </span>
                 <span className="h-full w-[4px] rounded-sm bg-main-color"></span>
                 <span>
-                  <GrLocation className=" text-xl text-dark-text/80" />
+                  <GrLocation className=" text-xl text-dark-text/80 dark:text-light-text/80" />
                 </span>
               </div>
               <div className="flex flex-col items-start justify-start gap-4">
                 {openedJourney.stops.map((stopName, index) => (
                   <p key={index} className="text-sm capitalize font-medium">
-                    <span className="text-dark-text/40">&bull; &nbsp; </span> {stopName}
+                    <span className="text-dark-text/40 dark:text-light-text">
+                      &bull; &nbsp;{" "}
+                    </span>{" "}
+                    {stopName}
                   </p>
                 ))}
               </div>
@@ -178,16 +181,20 @@ function MoreInfo({ hide, id }) {
               <span className="font-bold">{openedJourney.busType}</span>
             </h1>
             <h1 className="text-sm capitalize min-h-fit">
-              Price: <span className="font-bold">{openedJourney.price} rwf</span>
+              Price:{" "}
+              <span className="font-bold">{openedJourney.price} rwf</span>
             </h1>
-            <Link to={`/map`} className="min-h-[40px] w-full px-2 bg-main-color text-white font-semibold rounded-full text-sm flex items-center justify-center gap-1 cursor-pointer transition active:scale-95 ">
+            <Link
+              to={`/map`}
+              className="min-h-[40px] w-full px-2 bg-main-color text-white font-semibold rounded-full text-sm flex items-center justify-center gap-1 cursor-pointer transition active:scale-95 "
+            >
               <FaMapLocationDot className="text-xl" />
               Track Bus
             </Link>
           </div>
         )}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -19,7 +19,7 @@ function Notification({ hide }) {
   return (
     <>
       <div onClick={hideNotificationPopup} className={`w-full h-full fixed top-0 left-0 bg-stone-600/10 z-40 transition-all ease-in-out duration-300 ${animate ? 'opacity-100' : 'opacity-0'} `}></div>
-      <div className={`fixed top-0 right-0 bg-white text-dark-text w-full max-w-[500px] h-full z-40 p-4 transition-all ease-in-out duration-300 ${animate ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0 '}`}>
+      <div className={`fixed top-0 right-0 bg-white dark:bg-body-color-dark text-dark-text dark:text-light-text w-full max-w-[500px] h-full z-40 p-4 transition-all ease-in-out duration-300 ${animate ? 'translate-x-0 opacity-100' : 'translate-x-full opacity-0 '}`}>
         <div className="flex items-center justify-start gap-4">
           <button onClick={hideNotificationPopup} className="active:scale-75 transition flex items-center justify-center ">
             <IoArrowBackOutline className="text-2xl" />
@@ -29,8 +29,7 @@ function Notification({ hide }) {
 
         {/* no notification */}
         <div className="h-full w-full flex items-center justify-center flex-col gap-3 opacity-40">
-          <IoNotificationsOffOutline className="text-[90px] max-md:text-[60px]" />
-          <h1 className="text-lg tracking-tight font-bold pb-10">0 Notifications</h1>
+          <h1 className="text-sm tracking-tight font-semibold pb-10">No Notification</h1>
         </div>
       </div>
     </>
