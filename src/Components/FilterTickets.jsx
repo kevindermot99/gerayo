b54a977850ba23219c6c95b0b3d47d74b1edd25a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-21 22:36:37 +0200

    search excuse

--- src/Components/FilterTickets.jsx
+++ src/Components/FilterTickets.jsx
@@ -91,8 +91,11 @@ function FilterTickets({ onFilterSubmit, mobileSearch }) {
         }`}
       >
         <div className="w-full h-full absolute top-0 left-0 bg-stone-500/20 dark:bg-container-dark/60 rounded-[30px]"></div>
-        <p className="text-white/90 z-10 font-extrabold tracking-tighter text-3xl pb-2">
+        <p className="text-white z-10 font-extrabold tracking-tight text-4xl pb-1 ">
           Get yourself a seat!
+        </p>
+        <p className="text-white/90 z-10 font-medium text-sm pb-3">
+          {`If your ticket isn’t listed, it’s because we’re still working on it!`}
         </p>
         <form
           onSubmit={submit}
@@ -105,7 +108,7 @@ function FilterTickets({ onFilterSubmit, mobileSearch }) {
               type="text"
               onChange={checkEmptyFrom}
               value={inputValueFrom}
-              className="bg-white/70 backdrop-blur-md transition duration-300 focus:bg-white ring-0 ring-stone-200/50 dark:placeholder:text-dark-text h-full max-md:h-[40px] w-full capitalize max-w-[240px] max-md:max-w-[100%] rounded-full px-5 font-medium tracking-tight text-sm"
+              className="bg-white transition duration-300 ring-0 ring-stone-200/50 dark:placeholder:text-dark-text h-full max-md:h-[40px] w-full capitalize max-w-[240px] max-md:max-w-[100%] rounded-full px-5 font-medium tracking-tight text-sm"
             />
             <div
               className={`ring-1 ring-border-lines-light z-30 absolute top-[50px] p-1 w-full max-w-[240px] max-h-[200px] bg-white shadow-xl shadow-black/5  rounded-md overflow-clip flex flex-col ${
@@ -139,7 +142,7 @@ function FilterTickets({ onFilterSubmit, mobileSearch }) {
               type="text"
               onChange={checkEmptyTo}
               value={inputValueTo}
-              className="bg-white/70 backdrop-blur-md transition duration-300 focus:bg-white ring-0 ring-stone-200/50 dark:placeholder:text-dark-text h-full max-md:h-[40px] w-full capitalize max-w-[240px] max-md:max-w-[100%] rounded-full px-5 font-medium tracking-tight text-sm"
+              className="bg-white transition duration-300 ring-0 ring-stone-200/50 dark:placeholder:text-dark-text h-full max-md:h-[40px] w-full capitalize max-w-[240px] max-md:max-w-[100%] rounded-full px-5 font-medium tracking-tight text-sm"
             />
             <div
               className={`ring-1 ring-border-lines-light z-30 absolute top-[50px] p-1 w-full max-w-[240px] max-h-[200px] bg-white shadow-xl shadow-black/5  rounded-md overflow-clip flex flex-col ${
@@ -168,7 +171,7 @@ function FilterTickets({ onFilterSubmit, mobileSearch }) {
             </button>
             <button
               onClick={clearForm}
-              className={`h-full max-md:h-[40px] max-md:w-full  aspect-square bg-white bg-white/70 backdrop-blur-md cursor-pointer hover:bg-white ring-0 ring-stone-200 ml-2 rounded-full flex items-center justify-center text-red-400 font-semibold text-sm tracking-tight active:scale-90 whitespace-nowrap gap-1 transition-all ease-in-out duration-300 ${
+              className={`h-full max-md:h-[40px] max-md:w-full  aspect-square bg-white cursor-pointer ring-0 ring-stone-200 ml-2 rounded-full flex items-center justify-center text-red-400 font-semibold text-sm tracking-tight active:scale-90 whitespace-nowrap gap-1 transition-all ease-in-out duration-300 ${
                 formEmpty
                   ? "opacity-100"
                   : "md:opacity-0 md:pointer-events-none md:-z-10"
