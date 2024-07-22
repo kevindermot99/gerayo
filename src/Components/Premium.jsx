307f4242e9cb6f272c8b21e354d83da195c1181e
kevindermot99 <mbonimpayekevin@gmail.com>
2024-07-22 02:34:06 +0200

    premium 30% complete

--- src/Components/Premium.jsx
+++ src/Components/Premium.jsx
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
+import { CiLocationOn, CiRoute } from "react-icons/ci";
+import { GrLocationPin } from "react-icons/gr";
+import { PiBusLight, PiTicketLight } from "react-icons/pi";
 
-function Premium({ hidePremiumModal}) {
+function Premium({ hidePremiumModal }) {
   return (
     <>
-    {/* Premium Overlay */}
-    <div onClick={hidePremiumModal} className="fixed top-0 left-0 w-full h-full bg-black/40 z-[60]">
-    </div>
-    <div className='fixed top-0 left-0 right-0 bottom-0 m-auto h-[90%] max-h-[570px] w-[90%] max-w-[890px] bg-white z-[60] rounded-2xl flex flex-col overflow-clip'>
-    <div className='w-full h-auto flex-1 bg-sky-700 flex items-start justify-start'>
-        <div className='w-[40%] h-full'></div>
-        <div className='w-auto flex-1 h-full'></div>
-    </div>
-    <div className='w-full h-[70px] bg-sky-300'></div>
-    </div>
-
+      {/* Premium Overlay */}
+      <div
+        onClick={hidePremiumModal}
+        className="fixed top-0 left-0 w-full h-full bg-black/40 z-[60]"
+      ></div>
+      <div className="fixed top-0 left-0 right-0 bottom-0 m-auto h-[90%] max-h-[570px] w-[90%] max-w-[890px] bg-white dark:bg-body-color-dark z-[60] rounded-2xl shadow-2xl flex flex-col overflow-clip">
+        <div className="w-full h-full flex items-start justify-start overflow-clip dark:text-container-dark-3 text-3xl">
+          <div className="min-w-[350px] h-full bg-body-color-dark z-10 flex flex-col items-start justify-between">
+            <div className="w-full flex-1 h-auto">d</div>
+            <div className="w-full p-3 flex items-center justify-center">
+              <button className="text-sm w-full text-white bg-main-color p-2 px-3 rounded-xl">Upgrade for just $3/mo</button>
+            </div>
+          </div>
+          <div className="icons-bg-dark w-full h-full flex-1 flex flex-wrap items-center justify-center">
+            <div className="bg-body-color-dark h-28 aspect-square p-4 rounded-full flex items-center justify-center ">
+              <h1 className="font-extrabold tracking-tighter text-[30px]">PRO</h1>
+            </div>
+          </div>
+        </div>
+        <div className="w-full min-h-[100px]"></div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Premium
+export default Premium;
